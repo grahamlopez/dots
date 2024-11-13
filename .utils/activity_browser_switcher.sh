39422449ae0236b52b3575ad002acedf71c89eff
Graham Lopez <m477@duck.com>
2024-11-13 10:50:52 -0500

    updated browser switcher for plasma6

--- .utils/activity_browser_switcher.sh
+++ .utils/activity_browser_switcher.sh
@@ -6,9 +6,17 @@
 host=$(hostname)
 output_log_file="/tmp/activity_browser_switcher_${host}.log"
 
+# Plasma 5
+#monitor() {
+#  dbus-monitor --session \
+#    "destination=org.kde.ActivityManager,member=SetCurrentActivity"
+#}
+
+# Plasma 6
 monitor() {
   dbus-monitor --session \
-    "destination=org.kde.ActivityManager,member=SetCurrentActivity"
+    "type='signal',interface='org.kde.ActivityManager.Activities',member='CurrentActivityChanged'"
+
 }
 
 # helper function to turn a hash returned from dbus into an activity name
@@ -38,11 +46,12 @@ printf "$(date +%s), ${current_activity_name}, switch_to\n" >> ${output_log_file
 
 while read line
 do
+  echo "got $line"
+  echo ""
   case $line in
 
-    *"member=SetCurrentActivity"*)
-      read nextline
-      activity_string=$(echo ${nextline} | cut -d '"' -f 2)
+    *"string"*)
+      activity_string=$(echo ${line} | cut -d '"' -f 2)
       current_activity_name=$(get_activity_name ${activity_string})
       printf "$(date +%s), ${current_activity_name}, switch_to\n" >> ${output_log_file}
       if [[ "$current_activity_name" == "gentoo" ]]
@@ -55,6 +64,22 @@ do
         printf "bad state: unreachable branch\n"
       fi
       ;;
+
+    #*"member=SetCurrentActivity"*)
+    #  read nextline
+    #  activity_string=$(echo ${nextline} | cut -d '"' -f 2)
+    #  current_activity_name=$(get_activity_name ${activity_string})
+    #  printf "$(date +%s), ${current_activity_name}, switch_to\n" >> ${output_log_file}
+    #  if [[ "$current_activity_name" == "gentoo" ]]
+    #  then
+    #    xdg-settings set default-web-browser firefox-bin.desktop
+    #  elif [[ "$current_activity_name" == "nvidia" ]]
+    #  then
+    #    xdg-settings set default-web-browser microsoft-edge.desktop
+    #  else
+    #    printf "bad state: unreachable branch\n"
+    #  fi
+    #  ;;
 
   esac
 done < <(monitor)
