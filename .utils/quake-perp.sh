8b84ff8b7f085ef37243e8891504136695492519
Graham Lopez <m477@duck.com>
2025-05-15 22:57:23 -0400

    hyprland hacking

---
+++ .utils/quake-perp.sh
@@ -0,0 +1,49 @@
+#!/bin/env bash
+
+perp_workspace=$(hyprctl clients -j | jq -rec '.[] | select(.tags[] == "perp") | .workspace .name')
+
+# convert the tag to a window address
+perp_address=$(hyprctl clients -j | jq -rec '.[] | select(.tags[] == "perp" ) | .address')
+
+echo "perp currently on $perp_workspace"
+
+current_ws=$(hyprctl activeworkspace -j | jq '.id')
+
+echo "current workspace is ${current_ws}"
+
+if [ -z "$perp_workspace" ]; then
+
+	echo "start"
+
+  # open a new firefox window with perplexity
+	hyprctl dispatch -- exec "[float; size 1200 800; move 526 50]" "firefox-bin --new-window 'ext+container:name=Personal&url=https://perplexity.ai'"
+
+  sleep 0.2
+
+  # Create and move window to special workspace
+  hyprctl dispatch movetoworkspace special:perp
+
+  # tag the window for later tracking
+  hyprctl dispatch tagwindow +perp
+
+  # convert the tag to a window address
+  perp_address=$(hyprctl clients -j | jq -rec '.[] | select(.tags[] == "perp" ) | .address')
+
+  # bring the window to our current workspace and float
+	hyprctl dispatch movetoworkspace ${current_ws}, address:${perp_address}
+	hyprctl dispatch setfloating address:${perp_address}
+	hyprctl dispatch resizewindowpixel exact 1600 1200,address:${perp_address}
+	hyprctl dispatch movewindowpixel exact 326 50,address:${perp_address}
+
+elif [ "$perp_workspace" == "special:perp" ]; then
+
+	echo "show"
+	#hyprctl dispatch movetoworkspace "1,class:perp"
+	hyprctl dispatch movetoworkspace ${current_ws}, address:${perp_address}
+
+else
+
+	echo "hide"
+	hyprctl dispatch movetoworkspacesilent special:perp, address:${perp_address}
+
+fi
---
+++ .utils/quake-term.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+
+drop_workspace=$(hyprctl clients -j | jq -rec '.[] | select(.class == "dropdown") | .workspace .name')
+
+echo "dropdown currently on $drop_workspace"
+
+current_ws=$(hyprctl activeworkspace -j | jq '.id')
+
+if [ -z "$drop_workspace" ]; then
+
+	echo "start"
+	hyprctl dispatch -- exec "[float; size 1200 800; move 526 50]" kitty --class dropdown
+
+elif [ "$drop_workspace" == "special:dropdown" ]; then
+
+	echo "show"
+	#hyprctl dispatch movetoworkspace "1,class:dropdown"
+	hyprctl dispatch movetoworkspace "${current_ws},class:dropdown"
+
+else
+
+	echo "hide"
+	hyprctl dispatch movetoworkspacesilent "special:dropdown,class:dropdown"
+
+fi
