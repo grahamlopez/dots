c156953f3538c22dac386443a929bb93580dce7b
Graham Lopez <m477@duck.com>
2025-06-01 13:26:30 -0400

    tweaked hyprland and tmux startup

--- .utils/tmux-at-startup.sh
+++ .utils/tmux-at-startup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 tmux new-session -d -s 0 'sudo su -'   # First window runs your command, then stays open
-tmux new-window -t 0:1                 # Second window with default shell
-tmux attach-session -t 0
-
+tmux new-window -d -t 0:1              # Second window with default shell
