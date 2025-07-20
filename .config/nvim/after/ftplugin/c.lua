a659f74374af536c1319a6bfaea9b106cde42094
Graham Lopez <m477@duck.com>
2025-07-20 19:00:44 -0400

    added an ftplugin/c.lua

---
+++ .config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,12 @@
+
+vim.opt.foldmethod = "indent"
+vim.opt.tabstop = 2        -- Tab width
+vim.opt.shiftwidth = 2     -- Indent width
+vim.opt.softtabstop = 2    -- Soft tab width
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Smart autoindenting
+vim.opt.autoindent = true  -- Copy indent from current line
+vim.opt.breakindent = true -- Maintain indent when wrapping
+vim.opt.wrap = false       -- Don't wrap lines
+vim.opt.linebreak = true   -- Break at word boundaries if wrap enabled
+vim.opt.textwidth = 80     -- Text width for formatting
