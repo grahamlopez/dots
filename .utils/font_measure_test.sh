1bbd3a53548e99c9d7509c839de102a943bbf986
Graham Lopez <m477@duck.com>
2025-12-29 11:31:08 -0500

    small env adjustments

---
+++ .utils/font_measure_test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo 1
+echo 2
+echo 3
+echo 4
+echo 5
+echo 6
+echo 7
+echo 8
+echo 9
+echo 123456_10_123456_20_123456_30_123456_40_123456_50_123456_60_123456_70_123456_80_123456_90_12345_100_12345_110_12345_120_12345_130_12345_140_12345_150_12345_160_12345_170_12345_180_12345_190_12345_200
+echo 11
+echo 12
+echo 13
+echo 14
+echo 15 "Normal"
+echo -e 16 "\033[1mBold\033[22m"
+echo -e 17 "\033[3mItalic\033[23m"
+echo -e 18 "\033[3;1mBold Italic\033[0m"
+echo -e 19 "\033[4mUnderline\033[24m"
+echo -e 20 "== === !== >= <= =>"
+echo 21 "󰐊     󰄉      󰑓 󰒲 "
+echo 22
+echo 23
+echo 24
+echo 25
+echo 26
+echo 27
+echo 28
+echo 29
+echo 30
+echo 31
+echo 32
+echo 33
+echo 34
+echo 35
+echo 36
+echo 37
+echo 38
+echo 39
+echo 40
+echo 41
+echo 42
+echo 43
+echo 44
+echo 45
+echo 46
+echo 47
+echo 48
+echo 49
+echo 50
---
+++ .utils/quake-book.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+
+books_workspace=$(hyprctl clients -j | jq -rec '.[] | select(.class == "booksdown") | .workspace .name')
+
+echo "booksdown currently on $books_workspace"
+
+current_ws=$(hyprctl activeworkspace -j | jq '.id')
+
+if [ -z "$books_workspace" ]; then
+
+	echo "start"
+	hyprctl dispatch -- exec "[float; size 1200 800; move 226 500]" kitty --class booksdown -e nvim ~/Sync/notes/bookmarks.md
+
+elif [ "$books_workspace" == "special:booksdown" ]; then
+
+	echo "show"
+	#hyprctl dispatch movetoworkspace "1,class:booksdown"
+	hyprctl dispatch movetoworkspace "${current_ws},class:booksdown"
+
+else
+
+	echo "hide"
+	hyprctl dispatch movetoworkspacesilent "special:booksdown,class:booksdown"
+
+fi
