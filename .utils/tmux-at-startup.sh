ab1c704306c2e563cff216395f27ac6248528b3f
Graham Lopez <m477@duck.com>
2026-01-04 12:07:46 -0500

    local tmux for startup script

--- .utils/tmux-at-startup.sh
+++ .utils/tmux-at-startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tmux new-session -d -s 0 'sudo su -'   # First window runs your command, then stays open
-tmux new-window -d -t 0:1              # Second window with default shell
+/home/graham/local/bin/tmux new-session -d -s 0 'sudo su -'   # First window runs your command, then stays open
+/home/graham/local/bin/tmux new-window -d -t 0:1              # Second window with default shell
